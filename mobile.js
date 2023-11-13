2612402e99b91635922c3eac997fa86cb071ceb2
liuzhiyang <zhiyang1.liu@ximalaya.com>
2023-11-13 16:48:02 +0800

    feat: 增加注释

--- mobile.js
+++ mobile.js
@@ -6,11 +6,12 @@ module.exports = {
         "chrome 58"
     ],
     "production": [
-        ">1%",
-        "iOS 8",
-        "last 3 iOS versions",
+        ">1%",// 市占率超过1%的浏览器
+        "iOS 8", // 监听IOS 8的系统
+        "last 3 iOS versions", // 最近的三个
         "ie 10",
-        "since 2010"
+        "since 2010",
+        "not dead", // 24个月内没有官网支持或更新的浏览器
     ]
 }
 
